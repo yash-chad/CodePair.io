7ad56ce1b803c1f5dd3704428bb16ff2215cd737
Yash Chachad <chachadyash786@gmail.com>
2020-11-02 16:18:21 +0530

    Added route for updating a note

--- routes/notes.js
+++ routes/notes.js
@@ -26,6 +26,29 @@ router.post("/createNote", auth, async (req, res) => {
   );
 });
 
+// Update a Note
+router.put("/updateNote", auth, async (req, res) => {
+  if (!req.body.data || !req.body.note_id) {
+    return res.send({
+      Error: "Please insert all fields",
+    });
+  }
+  db.query(
+    `UPDATE NOTES SET DATA ="${req.body.data}" WHERE note_id=${req.body.note_id}`,
+    async (error, result) => {
+      if (error) {
+        return res.status(400).send({
+          Error: error,
+        });
+      } else {
+        return res.status(201).send({
+          success: "Note Updated",
+        });
+      }
+    }
+  );
+});
+
 //Gives all notes by the specific user in the requeted room
 router.get("/getNotes", auth, (req, res) => {
   if (!req.body.room_id) {
