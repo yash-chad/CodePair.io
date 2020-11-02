f25da532235897c1cfc44cd99a25d5facea80ce2
Yash Chachad <chachadyash786@gmail.com>
2020-11-02 17:17:22 +0530

    Updated createRoom route

--- routes/room.js
+++ routes/room.js
@@ -18,17 +18,33 @@ router.post("/createRoom", auth, async (req, res) => {
       Error: "Please insert all fields",
     });
   }
+
+  const room_id = parseInt(new Date().valueOf() / 10000);
+  // Creating a room Instance
   db.query(
-    `INSERT INTO ROOM (room_name,admin) VALUES("${req.body.room_name}","${req.user.user_id}")`,
+    `INSERT INTO ROOM (room_id,room_name,admin) VALUES("${room_id}","${req.body.room_name}",${req.user.user_id})`,
     async (error, result) => {
       if (error) {
         return res.status(400).send({
           Error: error,
         });
       } else {
-        return res.status(201).send({
-          success: "New room created",
-        });
+        // Adding the admin to the room
+        db.query(
+          `INSERT INTO UserRoomDetails(room_id,user_id) values(${room_id},${req.user.user_id});`,
+          (error, result) => {
+            if (error) {
+              res.send({
+                Error: error,
+              });
+            } else {
+              return res.status(201).send({
+                success: "New room created",
+                room_id,
+              });
+            }
+          }
+        );
       }
     }
   );
